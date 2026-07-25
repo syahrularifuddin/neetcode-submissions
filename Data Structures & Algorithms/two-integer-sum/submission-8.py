2ca8894553f18203f9ca05167b9872fe85ac61dc
Syahrul Arifuddin <98009725+syahrularifuddin@users.noreply.github.com>
2026-07-26 00:11:14 +0700

    Add: two-integer-sum - submission-8

---
+++ Data Structures & Algorithms/two-integer-sum/submission-8.py
@@ -0,0 +1,21 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+      cmap = {}
+      res = []
+      for i,n in enumerate(nums):
+        cmap[n] = i
+      v1,v2=0,0
+      print(nums)
+      print(cmap)
+      for i,n in enumerate(nums):
+        if target-n in cmap:
+          v1=i
+          idx = cmap.get(target-n)
+          if i == idx:
+              continue
+          v2=idx
+          break
+          print(v1,v2)
+      print(v1,v2)
+      return [v1,v2]
+      
