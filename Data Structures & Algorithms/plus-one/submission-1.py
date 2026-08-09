da46cedb23f2373f31cb0ebd0d47d1e9bad5387e
Syahrul Arifuddin <98009725+syahrularifuddin@users.noreply.github.com>
2026-08-09 17:48:45 +0700

    Add: plus-one - submission-1

---
+++ Data Structures & Algorithms/plus-one/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        t = 0
+        for d in digits:
+            t = (t*10)+d
+        t+=1
+        print(t)
+        res = []
+        while t>0:
+            res.insert(0, t%10)
+            t=t//10
+            print(t)
+        return res
